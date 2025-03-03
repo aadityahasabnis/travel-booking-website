76bbed526c81a24bc078e6ad94029600d06bc2de
Aaditya Hasabnis <aaditya.hasabnis@gmail.com>
2025-03-03 14:49:02 +0530

    New Changes

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing')
+const path = require('path');
+const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine('ejs', ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+
+// Logger
+// Middleware
+app.use((req, res, next)=> {
+    req.time = new Date(Date.now());
+    console.log(req.method);
+    console.log(req.hostname, req.time);
+    next();
+})
 
 
 // * MongoDB Connect Locally
@@ -21,12 +42,67 @@ main()
 
 
 
-    
+
 // * Home Route
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send("Hello")
 })
 
+
+// * Listings Route
+app.get('/listings', async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+})
+
+// * New Listing Page
+app.get("/listings/new", (req, res) => {
+    res.render('listings/new.ejs');
+})
+
+// * Post new listing
+app.post('/listings', async (req, res) => {
+    let { title, description, image: imageUrl, price, country, location } = req.body;
+    await Listing.insertOne({
+        title: title, description: description, image: { url: imageUrl }, price, country, location
+    })
+    res.redirect('/listings');
+})
+
+//  * Each Listing
+//  Show Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+})
+
+
+//  * Edit Each Listing in show
+//  Show Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+})
+
+// * Update listing
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let { title, description, image: imageUrl, price, country, location } = req.body;
+    await Listing.findByIdAndUpdate(id, {
+        title: title, description: description, image: { url: imageUrl }, price, country, location
+    }, { new: true })
+    res.redirect('/listings');
+})
+
+
+// * Delete Listing
+app.delete("/listings/:id/delete", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect('/listings');
+})
 
 // * Just for checking
 // app.get('/testlisting', async (req, res) => {
